9b1c05606f2d16e04e743d8c544557ffdabb9a20
Ruksana khatun <iitinaruksana@gmail.com>
2025-06-11 22:35:28 +0530

    Basic dsa

---
+++ first.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Ruksana khatn"<<endl;
+    cout<<"MongoDB today expoloring form harkirat"<<endl;
+    cout<<"IN MONGOOSE FIRST YOU HAVE TO DESCRIBE SCHEMA"<<endl;
+    cout<<"mongoose is forst step is to define schema"<<endl;
+     cout<<"user Schema emil password first name city"<<endl;
+     cout<<"emila  ruksana@gmail.com name:Ruksana pasword:340123"<<endl;
+     cout<<"in mongoDB SCHEMA THER A LOT TYPE OF Find"<<endl;
+     cout<<"find meanS all find() findOne findoneANDDelet findBYid findBYiANDDelete "<<endl;
+     cout<<"Dsa start here"<<endl;
+   //   int a,b;
+   //   cout<<'enter your a number';
+   //   cin>>a;
+   //   cout<<"enter your b number";
+   //   cin>>b;
+     float marks;
+     cout<<"enter your your marks of phy:";
+     cin>>marks;
+    //  cout<<'enter your marks of chemystry:';
+    //  cin>>marks;
+     if(marks==40){
+        cout<<"your pass inyour exmae";
+     }
+     else if(marks==80){
+        cout<<"your first devison in your exame"<<endl;
+     }
+     else if(marks==100){
+        cout<<"congrats you are got a good score in our exmas"<<endl;
+
+     }
+     else{
+        cout<<"you are fail";
+     }
+// int a,b;
+// cout<<"enter your a:";
+// cin>>a;
+// cout<<"enter your b;";
+// cin>>a;
+// char op;
+// cout<<"enter your op";
+// cin>>op;
+
+// if(op=='+'){
+//    cout<< "do (a+b)"<<a+b<<endl;
+// }else if(op=='-'){
+//    cout<<"d (a-b)"<<a-b<<endl;
+// }else if (op=='*'){
+//    cout<<"do a*b"<<a*b<<endl;
+// }else if(op=='/'){
+//    cout<<"Do (a/b)"<<a/b<<endl;
+// }else{
+//    cout<<"please provie s op"<<endl;
+// }
+
+for(int i=0;i<=10;i++){
+   cout<<i<<endl;
+
+}
+for(int i=0;i<5;i++){
+   for(int j=0;j<5;j++){
+      cout<<j<<endl;
+   }
+}cout<<endl<<"hello coder Army";
+int arr[5]={1,2,3,4,5};
+for(int i=0;i<5;i++){
+   cout<<arr[i]<<endl;
+}
+int arr1[5];
+for(int i=0;i<5;i++){
+   for(int j=0;j<5;j++){
+      cout<<arr1[j]<<",";
+   }
+   cout<<endl;
+} 
+int n=5;
+int arr4[n]={1,2,3,4,5};
+for(int i=0;i<5;i++){
+   for(int j=i;j<5;j++){
+      cout<<arr4[j]<<",";
+   }
+   cout<<endl;
+
+}
+
+
+return 0;
+     
+
+}
