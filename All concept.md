5db99f5610dc205c8ee92684ba6ade0c0893c595
Ruksana khatun <iitinaruksana@gmail.com>
2025-06-17 02:54:27 +0530

    explain in theroy

---
+++ All concept.md
@@ -0,0 +1,15 @@
+# Time and sapce complextity
+# Time complexity: depeand on only 
+# ___________________________
+# here is all array concept
+# int [A] this is called initilaztion of array
+# # int [A]={1,2,3,4,5,6} this is called initialization of array with values as you can sath ini... with deaclaratin
+# Refrence mean ike ek bar humne 1 define kiya thik hi ab ko humne dusra valu se define kr diya like int &r=a ye ho gya refrence agr tumen kl ko r++ kr diya a=bhi 1+1 -2 ho jayega aur r =2 ho jayga smjhi yarr
+# pass by value pass by refrence pass by adress
+# let's see hum tumhe sb smjhate h dekho ek bat phle function se shure krte hi function k hum isilye use krte hi isse se bhut faiyda hi smjhi kaise dekho function ko hum  ek barr likh do bar use kr skte hi aur waise agr hum int main ke ander sari chije likhe to bhut hi jayda code ho jayege aur thik nhi rhega to dusra faida ye bhi hi 
+# hume function ko ek barr likhne ke bad hum use kai bar use kr skte hi thik hi 
+ # dekho hum code likhte hi pta hi tumhe actual paramete  formal pramete aur prototype
+# MAIN PROTOTYPE DECLERATION OF A FUNCTION DEFINATION OF A FUNCTION ko hi ELABORATION KO KHTE HI 
+#  as you see all of thease in this sreen shot  ![alt text](image-1.png)
+
+ 
--- Array.cpp
+++ Array.cpp
@@ -1,9 +1,66 @@
 
 #include <iostream>
 using namespace std;
+
+// int LinearSearch(int arr[], int n, int key) {
+//     for (int i = 0; i < n; i++) {
+//         if (arr[i] == key) {
+//             return i; 
+//         }
+//     }
+//     return -1; 
+// }
+int add(int a,int b){
+    int c;
+    c=a+b;
+    return c;
+    cout<<"after retun it should not exectue"<<endl;
+
+}
 int main(){
+//     int arr[25]={1, 2, 3, 4, 5};
+//     int len=sizeof(arr)/sizeof(int);
+//   for(int i=0;i<len;i++){
+//    cin>>arr[i];
+   
+//   }
+//   for(int i=0;i<=len;i++){
+//     cout<<arr[i]<<",";
+//   }
+//   cout<<endl;
+
+// int array[]={114,15,16,8,90};
+// int n=sizeof(arr)/sizeof(int);
+// int max=array[0];
+// int min=array[0];
+// for(int i=0;i<=n;i++){
+//     if(array[i]>max){
+//         max=array[i];
+//       if(array[i]<min){
+//        min=array[i];
+        
+//     }
+//     }
+
   
-    int arr[5]={1, 2, 3, 4, 5};
-    cout<<arr[2]<<endl;
-    cout<<arr[4]<<endl; 
+
+// }
+// cout<<"max a value="<<max<<endl;
+// cout<<"min a value="<<min<<endl;
+//   int a=5;
+//   int *ptr=&a;
+//     cout<<"value of a="<<ptr<<endl;
+
+// linear search
+
+// int arr[]={1,2,3,4,5,6,7,8,9};
+// int n=sizeof(arr)/sizeof(int);
+// cout<<LinearSearch(arr, n, 9)<<endl;
+    
+//   return 0;
+int x,y,z;
+x=10;y=10;
+cout<<add(x,y)<<endl;
+return 0;
+    
 }
